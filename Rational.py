e6d10495a4761672e2b5e4d9c023277529d9353b
Arjun <arjunprahladan@gmail.com>
2016-05-18 13:10:10 +0530

    Adding arithmetic operators and overloading them

--- Rational.py
+++ Rational.py
@@ -2,7 +2,23 @@ class Rational:
 	def __init__(self,num,denom):
 		self.numerator = num
 		self.denominator = denom
-	def add(self,other):
+	def __add__(self,other):
+		# overloading + operator
 		newNumerator = self.numerator*other.denominator + other.numerator*self.denominator
 		newDenominator = self.denominator*other.denominator
+		return Rational(newNumerator,newDenominator)
+	def __sub__(self,other):
+		# overloading - operator
+                newNumerator = self.numerator*other.denominator - other.numerator*self.denominator
+                newDenominator = self.denominator*other.denominator
+                return Rational(newNumerator,newDenominator)
+	def __mul__(self,other):
+		# overloading * operator
+		newNumerator = self.numerator*other.numerator
+		newDenominator = self.denominator*other.denominator
+		return Rational(newNumerator,newDenominator)
+	def __truediv__(self,other):
+		# overloading / operator
+		newNumerator = self.numerator*other.denominator
+		newDenominator = self.denominator*other.numerator
 		return Rational(newNumerator,newDenominator)
